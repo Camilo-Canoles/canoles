912da364647560d93c9df35142847b0ae5c8dfdb
Camilo-Canoles <andresbetancourt2004@gmail.com>
2024-09-10 19:37:00 -0500

    Add files via upload

---
+++ App/TecnarGui/src/tecnar/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tecnar;
+
+import formulario.frmPersonas;
+
+/**
+ *
+ * @author estudiantes
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        frmPersonas P = new frmPersonas();
+        P.setVisible(true);
+        
+    }
+    
+}
